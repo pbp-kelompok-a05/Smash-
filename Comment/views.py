0c958b86d99843a6ff39c6aeb0ba4afe7124c9ea
nathanaeru <nathanael.leander@ui.ac.id>
2025-10-22 14:16:29 +0700

    adding comment views

--- Comment/views.py
+++ Comment/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from Comment.models import Comment
+from Comment.forms import CommentForm
+from Post.models import ForumPost
 
-# Create your views here.
+
+def show_comments(request, post_id):
+    comments = Comment.objects.filter(post__id=post_id).order_by("-created_at")
+    return render(request, "comments/comment_list.html", {"comments": comments})
+
+
+def add_comment(request, post_id):
+    post = get_object_or_404(ForumPost, id=post_id)
+    form = CommentForm(request.POST)
+    if form.is_valid() and request.method == "POST":
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.author = request.user
+        comment.save()
+        return redirect("show_comments", post_id=post.id)
+    context = {"form": form, "post": post}
+    return render(request, "comments/comment_form.html", context)
