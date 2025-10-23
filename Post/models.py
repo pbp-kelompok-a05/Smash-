e37a2314cdcb500d1663d19533e6c5b7c4226a42
Christna Yosua Rotinsulu <christna.yosua@ui.ac.id>
2025-10-23 10:47:46 +0700

    Menambahkan class PostLike di Modul Post untuk mempermudah pengambilan data

--- Post/models.py
+++ Post/models.py
@@ -1,14 +1,25 @@
+# Import library yang dibutuhkan 
+# UUID digunakan untuk primary key sebuah entitas
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 # Create your models here.
 class Category(models.Model):
     # Define attribute yang dibutuhkan
+    # Membuat nama dan deskripsi untuk kategori 
     name = models.CharField(max_length=255, verbose_name="Category Name")
     description = models.TextField(max_length=255, verbose_name="Description")
 
+    # Handle perilaku class Category 
+    # Membuat class meta untuk menyimpan metadata/opsi 
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
 
 # Class untuk Post ke Forum
 class ForumPost(models.Model):
@@ -28,32 +39,75 @@ class ForumPost(models.Model):
     # Content post -> Isi dari Post
     content = models.TextField(verbose_name="content")
     # Author atau penulis Post
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forum_posts", null=True)
+    # Category untuk post 
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Post Category")
     # Kategori post
-    category = models.CharField(
+    post_type = models.CharField(
         max_length=100,
         choices=post_kategory,
         default="category",
         verbose_name="Post Category",
     )
+
+    # Fields untuk media -> gambar dan video_url
+    image = models.ImageField(upload_to='forum_posts/images/', null=True, blank=True, verbose_name="Post Image")
+    video_url = models.URLField(max_length=500, null=True, blank=True, verbose_name="Video URL")
+
+    # Metadata untuk class ForumPost
     # Timestamp untuk pembuatan dan update post
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
     update_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
     # Additions
     is_pinned = models.BooleanField(default=False, verbose_name="Pinned 📌")
+    # Delete pin
+    is_deleted = models.BooleanField(default=False, verbose_name="Soft Deleted")
+
+    # Counters
     views = models.PositiveIntegerField(default=0, verbose_name="Views Count")
     likes = models.PositiveIntegerField(default=0, verbose_name="Likes Count")
     dislikes = models.PositiveIntegerField(default=0, verbose_name="Dislikes Count")
+
+    # Class Meta
+    class Meta:
+        ordering = ['-is_pinned', '-created_at']
+        verbose_name = "Forum Post"
+        verbose_name_plural = "Forum Posts"
 
     # Method tambahan
     # Return Post title
     def __str__(self):
         return self.title
 
+    # Handle jika user ingin mengirimkan dua media
+    def clean(self):
+        if self.image and self.video_url:
+            raise ValidationError("Post can have either an image or a video URL, not both!")
+
+    # Menghapus post forum
+    def soft_deleted(self):
+        self.is_deleted = True
+        self.save()
+
+    # User dapat mengembalikan postnya setelah dihapus
+    def restore(self):
+        self.is_deleted = False
+        self.save()
+
+    # Mendapatkan jumlah like dan dislike
+    def get_likes_count(self):
+        return self.likes.filter(is_like=True).count()
+
+    def get_dislikes_count(self):
+        return self.likes.filter(is_like=False).count()
+    
+    def get_comment_count(self):
+        return self.comments.filter(is_deleted=False).count()
+
     # Increment views
     def increment_views(self):
         self.views += 1
-        self.save()
+        self.save(update_fields=["views"])
 
     # Like post
     def like(self):
@@ -77,10 +131,6 @@ class ForumPost(models.Model):
             self.dislikes -= 1
             self.save()
 
-    # Get number of comments
-    def get_comment_count(self):
-        return self.comments.count()
-
     # Pin post
     def pin(self):
         self.is_pinned = True
@@ -90,3 +140,18 @@ class ForumPost(models.Model):
     def unpin(self):
         self.is_pinned = False
         self.save()
+
+# Class unutk PostLike -> Dipisah dari Forum Post untuk mempermudah proses pengambilan data
+class PostLike(models.Model):
+    # Membuat attribute yang dibutuhkan 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post = models.ForeignKey(ForumPost, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_like = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Membuat class Meta untuk handle perilaku PostLike
+    class Meta:
+        unique_together = ['post', 'user']
+        verbose_name = "Post Like"
+        verbose_name_plural = "Post Likes"
