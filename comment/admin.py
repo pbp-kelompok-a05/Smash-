6aab4b6c15b42d233b3e36275cb05ce06703b0f1
nathanaeru <nathanael.leander@ui.ac.id>
2025-10-24 19:26:07 +0700

    Making comments buttons functional

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Comment, CommentInteraction
 
 # Register your models here.
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "post",
+        "content",
+        "likes_count",
+        "dislikes_count",
+        "created_at",
+    ]
+    list_filter = ["created_at", "is_deleted"]
+    search_fields = ["content", "user__username"]
+
+
+@admin.register(CommentInteraction)
+class CommentInteractionAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "comment", "interaction_type", "created_at"]
+    list_filter = ["interaction_type", "created_at"]
+    search_fields = ["user__username", "comment__content"]
